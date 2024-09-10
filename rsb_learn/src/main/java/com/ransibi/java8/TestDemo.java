fb540bc32f554ad91fb18e4f5e4a175bca03456a
ransibi <rsb18252508590@163.com>
2024-09-10 17:04:43 +0800

    [修改]1.swagger-ui学习代码提交.

--- rsb_learn/src/main/java/com/ransibi/java8/TestDemo.java
+++ rsb_learn/src/main/java/com/ransibi/java8/TestDemo.java
@@ -1,22 +1,50 @@
 package com.ransibi.java8;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.text.Collator;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TestDemo {
     public static void main(String[] args) {
-        List<UserBean> userBeanList = new ArrayList<>();
-        UserBean userBean = new UserBean("小美", Arrays.asList("北京","贵州"));
-        UserBean userBean1 = new UserBean("小王", Arrays.asList("上海","湖南"));
-        UserBean userBean2 = new UserBean("小明", Arrays.asList("浙江","广东"));
-        userBeanList.add(userBean);
-        userBeanList.add(userBean1);
-        userBeanList.add(userBean2);
-        List<String> collect = userBeanList.stream().flatMap(user -> user.getAddress().stream()).collect(Collectors.toList());
-        System.out.println(collect);
+//        List<UserBean> userBeanList = new ArrayList<>();
+//        UserBean userBean = new UserBean("小美", Arrays.asList("北京","贵州"));
+//        UserBean userBean1 = new UserBean("小王", Arrays.asList("上海","湖南"));
+//        UserBean userBean2 = new UserBean("小明", Arrays.asList("浙江","广东"));
+//        userBeanList.add(userBean);
+//        userBeanList.add(userBean1);
+//        userBeanList.add(userBean2);
+//        List<String> collect = userBeanList.stream().flatMap(user -> user.getAddress().stream()).collect(Collectors.toList());
+//        System.out.println(collect);
+
+//        List<String> strings1 = Arrays.asList("abc", "abd", "aba", "efg", "abcd", "jkl", "jkl");
+//        List<String> strings2 = Arrays.asList("张三", "李四", "王五", "赵柳", "张哥", "李哥", "王哥");
+//        List<Integer> strings3 = Arrays.asList(10, 2, 30, 22, 1, 0, -9);
+//        List<String> sorted1 = strings1.stream()
+//                .sorted()
+//                .collect(Collectors.toList());
+//        List<String> sorted2 = strings2.stream()
+//                .sorted(Collections.reverseOrder(Collator.getInstance(Locale.CHINA)))
+//                .collect(Collectors.toList());
+//        List<Integer> sorted3 = strings3.stream()
+//                .sorted()
+//                .collect(Collectors.toList());
+//        System.out.println(sorted1);
+//        System.out.println(sorted2);
+//        System.out.println(sorted3);
+//        List<String> strings = Arrays.asList("abc", "abc", "bc", "efg", "abcd", "jkl", "jkl");
+//        List<String> flatMapLst = strings.stream().flatMap(str -> Arrays.stream(str.split(""))).collect(Collectors.toList());
+//        // 将字符串列表转换为字符列表
+//        List<Character> mapLst = strings.stream().flatMap(s -> s.chars().mapToObj(c -> (char) c)).collect(Collectors.toList());
+//        System.out.println(flatMapLst);
+//        System.out.println(mapLst);
+//        List<String> strings = Arrays.asList("cv", "abd", "aba", "efg", "abcd", "jkl", "jkl");
+//        long count = strings.stream().count();
+//        System.out.println(count);
+
+        List<String> tempLst = new ArrayList<>();
+        tempLst.stream().forEach(obj -> System.out.println("===="));
 
     }
 }
