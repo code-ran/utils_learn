d96cd0b5ce54918134dfc38be1a22022bfc740a9
ransibi <rsb18252508590@163.com>
2024-09-10 17:05:11 +0800

    [修改]1.swagger-ui学习代码提交.

---
+++ swagger_ui/src/main/java/com/ransibi/Main.java
@@ -0,0 +1,14 @@
+package com.ransibi;
+
+/**
+ * @description: ${description}
+ * @author: rsb
+ * @description: 2024-09-10-17-01
+ * @description: ${description}
+ * @Version: 1.0.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
